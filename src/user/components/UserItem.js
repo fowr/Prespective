e7cc12a417ee7582d50bbf152dfa1966b4d9c0de
soheil rahsaz <soheilrahsaz.sr@gmail.com>
2021-07-04 11:26:10 +0430

    Change Users list

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -2,28 +2,24 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 import Avatar from '../../shared/components/UIElements/Avatar'
-import Card from '../../shared/components/UIElements/Card'
 import Button from "../../shared/components/FormElement/Button"
 
 import { FiUser ,FiImage} from "react-icons/fi";
 import './UserItem.scss';
 const UserItem=props => {
-    return (
-    <div className="user-item col-lg-4 col-md-6 col-sm-8 col-10">
-    <Card className="user-item__content">
-        <Link to={`/${props.id}/photos`}>
-        <div className="user-item__image">
-           <Avatar image={props.image} alt={props.name}/>
-        </div>
-        <div className="user-item__info">
-            <h2><span><FiUser/></span>{props.name}</h2>
-      
-        <h3> <span><FiImage/></span>{props.placeCount } پست</h3>
-        </div>
-       
-        </Link>
-        <Button inverse>دنبال کردن</Button>
-    </Card>
+ return (  <div className=" col-lg-3 col-md-4 col-sm-10 ">
+        <li className="user-item">
+            <Link to={`/${props.id}/photos`}>
+            <div className="user-item__image">
+            <img src={props.image} alt={props.name}/>
+            </div>
+            <div className="user-item__info">
+                <h2><span><FiUser/></span>{props.name}</h2>
+                 <h2> <span><FiImage/></span>{props.placeCount } پست</h2>
+            <button className='user-item__btn' >مشاهده پروفایل</button>
+            </div>
+            </Link>
+        </li>
     </div>)
 };
 
